6d5e27ded55e37f1ad6195dfc02715d2094331d8
shivam <shivamti1536@gmail.com>
2024-04-10 20:44:46 +0530

    server change

--- Config/db.js
+++ Config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 function connectionDb() {
   mongoose
     .connect(
-      "mongodb+srv://shivam:k344MirCBdLcq3Fo@cluster0.ga7oh4z.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://shivam:zNNI6DieNmr03FmO@cluster0.ga7oh4z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     )
     .then(() => {
       console.log("db conctated succesfully...");
