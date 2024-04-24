9747769affde5bce2b9e0df2f98a2876c5c97863
shivam <shivamti1536@gmail.com>
2024-04-24 22:35:58 +0530

    hello

--- Controller/register.js
+++ Controller/register.js
@@ -3,7 +3,6 @@ const Register = require("../Models/register");
 async function register(req, res) {
   const { first_name, last_name, email, number, password, re_password } =
     req.body;
-console.log(req.body);
   Register.findOne({ email: email })
     .then((result) => {
       if (result?.email === email) {
@@ -36,6 +35,65 @@ console.log(req.body);
     });
 }
 
+async function updateRegister(req, res) {
+  const id = req.query._id;
+  const {  first_name, last_name, email, number, password, re_password } = req.body;
+  
+  // Find the existing document with the given ID
+  Register.findById(id)
+    .then((existingUser) => {
+      if (!existingUser) {
+        return res.status(404).json({ msg: "User not found", response_code: 404 });
+      }
+
+      // Update the existing document with new data
+      existingUser.first_name = first_name;
+      existingUser.last_name = last_name;
+      existingUser.email = email;
+      existingUser.number = number;
+      existingUser.password = password;
+      existingUser.re_password = re_password;
+
+      // Save the updated document
+      existingUser.save()
+        .then(() => {
+          res.status(200).json({ msg: "User updated successfully", response_code: 200 });
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).json({ msg: "Internal server error", response_code: 500 });
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ msg: "Internal server error", response_code: 500 });
+    });
+}
+
+
+
+
+async function deleteRegister(req, res) {
+  const id = req.query._id;
+  console.log(id);
+  Register.deleteOne({_id:id})
+    .then((category) => {
+      if (category) {
+        res
+          .status(200)
+          .json({
+            msg: " Register Sueess... !",
+            count:category.length,
+            data: category,
+            response_code: 200,
+          });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
 async function getRegister(req, res) {
   Register.find()
     .then((category) => {
@@ -55,4 +113,4 @@ async function getRegister(req, res) {
     });
 }
 
-module.exports = { register, getRegister };
+module.exports = { register, getRegister,deleteRegister,updateRegister };
--- Router/Router.js
+++ Router/Router.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register, getRegister } = require("../Controller/register");
+const { register, getRegister, deleteRegister, updateRegister } = require("../Controller/register");
 const { OtpSend, OtpVerifay } = require("../Controller/otp");
 
 const router = new express.Router();
@@ -8,4 +8,6 @@ router.post("/register", register);
 router.post("/get_user",getRegister)
 router.post("/send-otp", OtpSend);
 router.post("/otp_verifay", OtpVerifay);
+router.post("/delete_user/", deleteRegister);
+router.post("/update_register/", updateRegister);
 module.exports = router;
